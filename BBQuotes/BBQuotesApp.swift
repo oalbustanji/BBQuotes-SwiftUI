abbcf0aeb5c9c7bac77e7c59c84b4c07f75b4c34
omaralbutanji <omar.albustanji@alsafeerit.com>
2025-09-20 12:44:32 +0300

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Omar Bustanji on 20/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
